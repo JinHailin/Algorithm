aa6334dd2e3497cf1489b04132680e745eb18dc6
Jin Hailin <jinhailin1997@gmail.com>
2018-04-08 14:32:05 +0800

    Update selection_sort.cpp

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -5,18 +5,17 @@ int main()
 	int arr[N] = { 1,4,6,2,3,9,12,2,4,5 };
 	for (int i = 0, min; i < N - 1; i++)
 	{
-		min = i;
+		min = i;				// 寻找最小值的下标
 		for (int j = i; j < N; j++)
 		{
-			if (arr[j] < arr[min])
+			if (arr[j] < arr[min])		// 遍历、比较
 				min = j;
 		}
-		int temp = arr[i];
+		int temp = arr[i];			// 与min进行交换
 		arr[i] = arr[min];
 		arr[min] = temp;
 	}
 	for (int i = 0; i < N; i++)
 		std::cout << arr[i] << " ";
 	return 0;
-
 }
